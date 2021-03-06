657ef0c17a110118a966a5afd1109ae44d2a6666
hitrex <80149442+hitrex@users.noreply.github.com>
2021-03-06 12:04:04 +0330

    code-filteri-rubik.py

--- code-filter.py
+++ code-filter.py
@@ -1,1 +1,31 @@
 # code-filter
+from time import sleep
+
+
+print("###################################")
+print("|                                 |")
+print("|                                 |")
+print("|    code filteri          ‌|")
+print("|                                 |")
+print("|                                 |")
+print("|---------------------------------|")
+print("|                                 |")
+print("|   Manufacturer : hitrex      |")
+print("|cr:                |")
+print("|             hitrex  |")
+print("###################################")
+
+
+hack = input("id Channel : ")
+
+l = ["code the rubika 18%...", "code the rubika 32%....", 
+"code the rubika 58%.....", 
+"code the rubika 76%......", 
+"code the rubika 99%.......", " * code filteri rubika - Done! Crack the code  rubika code filter :* "]
+
+for i in l:
+    print("\r" + i, end="")
+    sleep(1.5)
+print()
+
+print("code filter: dilter⅛*o" or "cge/fil" or "¾⁷⅜filfil" or "filter/&⅝⁸" )
